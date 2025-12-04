96faf4abd89780869be52ce5a20dff64d7de78e4
Rigbin56 <rigbin.56@gmail.com>
2025-12-04 18:21:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Botón flotante
+document.getElementById("floatBtn").addEventListener("click", () => {
+    let menu = document.getElementById("menuFlotante");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
+
+// Carrusel info (solo abre alert con texto)
+function go(lizzy) {
+    alert("Información de: " + Lizzy + "\n(skibidi tessa milanesa)");
+}
+
+//agregar los demas personajes JAJAJAJ
